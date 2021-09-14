f0dbd2bcb94d78b9f91977271f33d72b93c131e5
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-09-14 13:45:51 +0900

    Create useEffect.js

---
+++ useEffect/useEffect.js
@@ -0,0 +1,24 @@
+import { StrictMode, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, []);
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
