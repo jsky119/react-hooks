8e59ca90f0aac1d926b599b898dd5288a09ed57c
jsky119 <87289235+jsky119@users.noreply.github.com>
2021-09-22 21:47:47 +0900

    Create useNetwork.js

---
+++ useNetwork/useNetwork.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+  const handleChange = () => {
+    if (typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+    () => {
+      window.removeEvnetListener("online", handleChange);
+      window.removeEvnetListener("offline", handleChange);
+    };
+  }, []);
+  return status;
+};
+
+const App = () => {
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "We are offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
+  return (
+    <div className="App">
+      <h1>{onLine ? "Online" : "offline"}</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
